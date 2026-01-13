1031bf3687a02bd770e2505535c567b1cfe9f282
Pablo <pgac247@learner.dvusd.org>
2026-01-13 13:54:22 -0700

    middle proggress

--- Py Fest/Py Fest/Py_Fest.py
+++ Py Fest/Py Fest/Py_Fest.py
@@ -6,20 +6,33 @@ print("4. Remove a Band by Name")
 print("5. Move Band to a specific position")
 print("6.Exit")
 
-choice = input("Enter your choice (1-6): ")
+lineup = [ 
+    ("name": "band_name", "time": 45),
+    ("name": "band_name", "time": 30),
+    ("name": "band_name", "time": 40),
+    ("name": "band_name", "time": 25),
+    ("name": "band_name", "time": 35)
+]
 
-if choice == "1":
-    print("\n---Current Lineup---")
+while True:
+    
+    choice = input("Enter your choice (1-6): ")
 
-elif choice == "2":
-    band_name = input("Enter the name of the new band: ")
-    band_time = int(input("Enter the performance time (in minutes): "))
-    print(f"Added {band_name} with a performance time of {band_time} minutes.")
+    if choice == "1":
+        print("\n---Current Lineup---")
+        print(lineup)
 
-elif choice == "3":
-    num_bands = int(input("Enter the number of bands to move to the end: "))
-    print(f"Moved the first {num_bands} bands to the end of the lineup.")
+    elif choice == "2":
+        band_name = input("Enter the name of the new band: ")
+        band_time = int(input("Enter the performance time (in minutes): "))
+        print(f"Added {band_name} with a performance time of {band_time} minutes.")
 
-elif choice == "4":
-    band_name = input("Enter the name of the band to remove: ")
-    print(f"Removed {band_name} from the lineup.")
+    elif choice == "3":
+        bands_name = int(input("Enter the name of bands to move to the end: "))
+        print(f"Moved the first {bands_name} bands to the end of the lineup.")
+
+    elif choice == "4":
+        band_name = input("Enter the name of the band to remove: ")
+        print(f"Removed {band_name} from the lineup.")
+
+
